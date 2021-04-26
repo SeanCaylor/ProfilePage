0c0eef89ff30632c6af76d8d9a82caf9623f5939
SeanCaylor <seanpcaylor@gmail.com>
2021-04-26 15:17:06 -0700

    update

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function photoChango(){
     var image = document.getElementById("profilePic");
+    var link = document.getElementById("itsASecret");
     if (image.getAttribute('src') == "img/ProfilePic.png"){
         image.src = "img/JBFH.png"
+        link.href = "./x_Insane personal experiment/index.html"
     }
     else {
         image.src = "img/ProfilePic.png"
+        link.href = "#"
     }
 }
